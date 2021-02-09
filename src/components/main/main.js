b2b268c9496b9a92eb9146a6183324dacd0419b5
AndreiMilashevich <milashevichandrei@gmai.com>
2021-02-10 01:55:12 +0300

    feat: create new branch

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,7 +1,15 @@
 import './main.scss';
+import Message from '../message/message'
 
 function Main() {
-  return <div className="w-100 bg-info block-main"></div>
+  return <div className="w-100 bg-info block-main">
+    <Message />
+    <Message />
+    <Message />
+    <Message />
+    <Message />
+    <Message />
+  </div>
 };
 
 export default Main;
---
+++ src/components/message/message.js
@@ -0,0 +1,7 @@
+import './message.scss';
+
+function Message() {
+  return <div className="user_message"> user text</div>
+};
+
+export default Message;
